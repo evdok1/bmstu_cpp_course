a896e54cabaf5baca117ea149727619cac75672a
vboxuser <evdokimovamsk@yandex.ru>
2025-12-27 13:44:10 +0300

    test

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -12,49 +12,94 @@ class stack
    public:
 	stack() : data_(nullptr), size_(0u) {}
 
-	bool empty() const noexcept { return size_ == 0; }
+	bool empty() const noexcept { return size_ == 0u; }
 
 	size_t size() const noexcept { return size_; }
 
-	~stack() {
-		if (data_!= nullptr) {
-			operator delete(data_);
+	stack(const stack& other) : data_(nullptr), size_(0u)  // konstr kop
+	{
+		size_ = other.size_;
+
+		data_ = (T*)(operator new(sizeof(T) * other.size_));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (data_ + i) T(other.data_[i]);
 		}
 	}
+	stack(stack&& other) : data_(nullptr), size_(0u)  // konst perem
+	{
+		data_ = other.data_;
+		size_ = other.size_;
+		other.data_ = nullptr;
+		other.size_ = 0;
+	}
+	stack& operator=(const stack& other)
+	{
+		if (this != &other)
+		{
+			clear();
+			size_ = other.size_;
 
+			data_ = (T*)(operator new(sizeof(T) * size_));
+			for (size_t i = 0; i < size_; ++i)
+			{
+				new (data_ + i) T(other.data_[i]);
+			}
+			// perem kop
+		}
+		return *this;
+	}
+
+	stack& operator=(stack&& other)
+	{
+		if (this != &other)
+		{
+			clear();
+			data_ = other.data_;
+			size_ = other.size_;
+			other.data_ = nullptr;
+			other.size_ = 0;
+		}
+		return *this;
+	}
+
+	~stack() { clear(); }
+
+	T* data() const { return data_; }
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
-		T * new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
-
-		for (size_t i=0; i< size_; i++) {
+		T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+		for (size_t i = 0; i < size_; ++i)
+		{
 			new (&new_data[i]) T(std::move(data_[i]));
 			data_[i].~T();
 		}
 		new (&new_data[size_]) T(std::forward<Args>(args)...);
 		operator delete(data_);
-
 		data_ = new_data;
 		++size_;
 	}
 
-	void push(T&& value) {
-		T * new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+	void push(T&& value)
+	{
+		T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
 
-		for (size_t i=0; i< size_; ++i) {
+		for (size_t i = 0; i < size_; ++i)
+		{
 			new (&new_data[i]) T(std::move(data_[i]));
 			data_[i].~T();
 		}
-
-		new (&new_data[size_]) T(std::forward<T>(value));
+		new (&new_data[size_]) T(std::move(value));
 		operator delete(data_);
-
 		data_ = new_data;
 		++size_;
 	}
 
-	void clear() noexcept {
-		for (size_t i=0; i<size_; ++i) {
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
 			data_[i].~T();
 		}
 		operator delete(data_);
@@ -62,40 +107,46 @@ class stack
 		size_ = 0;
 	}
 
-	void push(const T& value) {
-		T * new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
-
-		for (size_t i=0; i<size_; ++i) {
+	void push(const T& value)
+	{
+		T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+		for (size_t i = 0; i < size_; ++i)
+		{
 			new (&new_data[i]) T(std::move(data_[i]));
 			data_[i].~T();
 		}
-		new(&new_data[size_]) T(value);
+		new (&new_data[size_]) T(value);
 		operator delete(data_);
-
 		data_ = new_data;
 		++size_;
 	}
 
-	void pop() {
-		if (empty()) {
-			throw std::underflow_error("Stack is emply");
+	void pop()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
 		}
 		--size_;
 		data_[size_].~T();
 	}
 
-	T& top() { 
-		if (empty()) {
-			throw std::underflow_error("Stack is empty");
+	T& top()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
 		}
-		return data_[size_ -1]; 
+		return data_[size_ - 1];
 	}
 
-	const T& top() const {
-		if (empty()) {
-			throw std::underflow_error("Stack is empty");
+	const T& top() const
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
 		}
-		return data_[size_ -1]; 
+		return data_[size_ - 1];
 	}
 
    private:
--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -20,9 +20,11 @@ class simple_basic_string
 	/// Конструктор по умолчанию
 	simple_basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
-	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size) {
+	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size)
+	{
 		T space = T(' ');
-		for (auto i=0; i < size_; ++i) {
+		for (auto i = 0; i < size_; ++i)
+		{
 			ptr_[i] = space;
 		}
 		ptr_[size_] = T(0);
@@ -31,7 +33,8 @@ class simple_basic_string
 	simple_basic_string(std::initializer_list<T> il)
 		: ptr_(new T[il.size() + 1]), size_(il.size())
 	{
-		for (auto i = 0u; i < il.size(); ++i) {
+		for (auto i = 0u; i < il.size(); ++i)
+		{
 			*(ptr_ + i) = *(il.begin() + i)
 		}
 		size_ = il.size();
@@ -39,29 +42,33 @@ class simple_basic_string
 	}
 
 	/// Конструктор с параметром си-с
-	simple_basic_string(const T* c_str) {
-		auto len = strlen(const T* c_str)
-		ptr_ = new T[len + 1];
+	simple_basic_string(const T* c_str)
+	{
+		auto len = strlen(const T* c_str) ptr_ = new T[len + 1];
 		size_ = len;
 		ptr_[size_] = T(0);
-		for (auto i = 0u; i < size_; ++i) {
+		for (auto i = 0u; i < size_; ++i)
+		{
 			*(ptr_ + i) = *(c_str + i);
 		}
 	}
 
 	/// Конструктор копирования
-	simple_basic_string(const simple_basic_string& other) {
+	simple_basic_string(const simple_basic_string& other)
+	{
 		ptr_ = new T[other.size() + 1];
 		size_ = other.size_;
 		ptr_[size_] = T(0);
 
-		for (auto i = 0u; i < size_; ++i) {
+		for (auto i = 0u; i < size_; ++i)
+		{
 			*(ptr_ + i) = *(other.ptr_ + i)
 		}
 	}
 
 	/// Перемещающий конструктор
-	simple_basic_string(simple_basic_string&& dying) {
+	simple_basic_string(simple_basic_string&& other)
+	{
 		delete[] ptr_;
 
 		ptr_ = other.ptr_;
@@ -73,9 +80,7 @@ class simple_basic_string
 	}
 
 	/// Деструктор
-	~simple_basic_string() {
-		delete[] ptr_;
-	}
+	~simple_basic_string() { delete[] ptr_; }
 
 	/// Геттер на си-строку
 	const T* c_str() const { return ptr_; }
@@ -86,7 +91,7 @@ class simple_basic_string
 	simple_basic_string& operator=(simple_basic_string&& other)
 	{
 		delete[] ptr_;
-		
+
 		return *this;
 	}
 
